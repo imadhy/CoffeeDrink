b9d16916c6bf5c0a5e0f7f633644df2fc38209d7
imadhy <imadhy.gt@gmail.com>
2015-12-04 12:12:27 +0100

    MAJ ok

--- CoffeDrink/src/main/java/com/univ/alma/mw/backend.java
+++ CoffeDrink/src/main/java/com/univ/alma/mw/backend.java
@@ -3,44 +3,38 @@ package com.univ.alma.mw;
 import java.util.UUID;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Path;
-import javax.ws.rs.POST;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-
+@Path("")
 public class backend {
 
-    private static final String authServer = "http://localhost:8080";
-	private static final String REGEX_UUID = "[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[34][0-9a-fA-F]{3}-[8-9a-b][0-9a-fA-F]{3}-[0-9a-fA-F]{12}";
-
-	@POST
+	@GET
 	@Path("bonjour")
-	@Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
 	public Response bonjour() {
 
 		int status = 200;
         String res = "[401] J'te connais pas va t'authentifier";
 
+
 		return Response
 			.status(status)
-			.type(MediaType.TEXT_PLAIN)
+			.type(MediaType.APPLICATION_JSON)
 			.entity(res)
 			.build();
 	}
 
-
-
     @POST
     @Path("callback")
+    @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.APPLICATION_JSON)
     public Response callback(String token) {
 
         int status = 200;
         String res = null;
-        if(token.matches(REGEX_UUID)) {
+        if(token.matches()) {
 
         } else {
 
@@ -51,5 +45,4 @@ public class backend {
                 .entity(res)
                 .build();
     }
-
 }
