8ddda8a87cc80c4ccd1d5201898dfb0ea3d0c330
imadhy <imadhy.gt@gmail.com>
2015-12-04 11:35:58 +0100

    MAJ

--- CoffeDrink/src/main/java/com/univ/alma/mw/backend.java
+++ CoffeDrink/src/main/java/com/univ/alma/mw/backend.java
@@ -4,52 +4,52 @@ import java.util.UUID;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 
-import org.alma.middleware.bearbouncer.Identity;
-import org.alma.middleware.bearbouncer.Storage;
-
-import com.google.gson.Gson;
 
 public class backend {
-	
+
+    private static final String authServer = "http://localhost:8080";
+	private static final String REGEX_UUID = "[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[34][0-9a-fA-F]{3}-[8-9a-b][0-9a-fA-F]{3}-[0-9a-fA-F]{12}";
+
 	@POST
 	@Path("bonjour")
 	@Consumes(MediaType.TEXT_PLAIN)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response bonjour(String token) {
-		Storage db = new Storage();
-		
+	public Response bonjour() {
+
 		int status = 200;
-		String res = null;
-		if(imei.matches(REGEX_IMEI)) {
-			Identity identity = db.getIdentity(imei);
-			if(identity != null) {
-				status = HttpServletResponse.SC_OK;
-				res = UUID.randomUUID().toString();
-				db.putToken(res,imei);
-			} else {
-				status = HttpServletResponse.SC_NOT_FOUND;
-				res = "No identity corresponds to IMEI '"+imei+"'";
-			}
-		} else {
-			status = HttpServletResponse.SC_BAD_REQUEST;
-			res = "Content is not a well formed IMEI.";
-		}
-		db.close();
+        String res = "[401] J'te connais pas va t'authentifier";
+
 		return Response
 			.status(status)
 			.type(MediaType.TEXT_PLAIN)
 			.entity(res)
 			.build();
 	}
+
+
+
+    @POST
+    @Path("callback")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response callback(String token) {
+
+        int status = 200;
+        String res = null;
+        if(token.matches(REGEX_UUID)) {
+
+        } else {
+
+        }
+        return Response
+                .status(status)
+                .type(MediaType.TEXT_PLAIN)
+                .entity(res)
+                .build();
+    }
 
 }
